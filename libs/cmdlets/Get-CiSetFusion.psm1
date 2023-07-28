307eddd35cf88345039fc68cad4c14e5b0464827
sgkens <sgkens@snowlab.tk>
2023-07-28 21:35:05 +1000

    🎨 Improve: ( Get-CiSetFusion ) Added addtional parameter switches `-NoSemver`, `-Major`, `-Minor`, `-Patch`
    📜 NOTES:
    
    ✏  -NoSemVer: Returns CiSet items that have a type value of ''
    ✏  -Major: Returns CiSet Items That have Type Value of 'Major'
    ✏  -Minor: Returns CiSet items that have a type value of 'Minor'
    ✏  -Patch: Returns CiSet items that have a type value of 'Patch'
    
    👤 @sgkens 📅 9:33PM, Friday 28th July 2023

--- libs/cmdlets/Get-CiSetFusion.psm1
+++ libs/cmdlets/Get-CiSetFusion.psm1
@@ -2,9 +2,54 @@ Function Get-CiSetFusion() {
     [CmdletBinding()]
     [OutputType([psobject])]
     [Alias("gcsf")]
-    param ()
-    process{
-        (Get-CommitFusionModuleInstance).GetCiSetFusion()
+    param (
+        [parameter(
+            Mandatory = $false,
+            Position = 0,
+            ValueFromPipeline = $true,
+            HelpMessage = "Will Return all CiSets that do not a semver type"
+        )]
+        [switch]$NoSemver,
+        [parameter(
+            Mandatory = $false,
+            Position = 1,
+            ValueFromPipeline = $true,
+            HelpMessage = "Will Return all CiSets that have a semver of Patch"
+        )]
+        [switch]$Patch,
+        [parameter(
+            Mandatory = $false,
+            Position = 2,
+            ValueFromPipeline = $true,
+            HelpMessage = "Will Return all CiSets that have a semver of Minor"
+        )]
+        [switch]$Minor,
+        [parameter(
+            Mandatory = $false,
+            Position = 3,
+            ValueFromPipeline = $true,
+            HelpMessage = "Will Return all CiSets that have a semver of Major"
+        )]
+        [switch]$Major
+    )
+    process {
+        if (!$noSEMVer -and !$Patch -and !$Minor -and !$major) {
+            (Get-CommitFusionModuleInstance).GetCiSetFusion() ## Format-table returns a different object type so we can't use it here
+        }
+        else {
+            if ($noSEMVer) {
+                (Get-CommitFusionModuleInstance).GetCiSetFusion() | Where-Object { $_.semver.length -eq 0 -or $_.semver -eq "" } | Format-Table -Wrap -AutoSize
+            }
+            if ($Patch) {
+                (Get-CommitFusionModuleInstance).GetCiSetFusion() | Where-Object { $_.semver.length -ne 0 -and $_.semver.trim() -match "patch" } | Format-Table -Wrap -AutoSize
+            }
+            if ($Minor) {
+                (Get-CommitFusionModuleInstance).GetCiSetFusion() | Where-Object { $_.semver.length -ne 0 -and $_.semver.trim() -match "Minor" } | Format-Table -Wrap -AutoSize
+            }
+            if ($major) {
+                (Get-CommitFusionModuleInstance).GetCiSetFusion() | Where-Object { $_.semver.length -ne 0 -and $_.semver.trim() -match "major" } | Format-Table -Wrap -AutoSize
+            }
+        }
     }
 }
 Export-ModuleMember -Function Get-CiSetFusion
