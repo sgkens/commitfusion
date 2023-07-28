29c6e56e98ad63fc14601e7736314ccb4bddeb2d
sgkens <sgkens@snowlab.tk>
2023-07-29 01:03:26 +1000

    🎨 Improve(cmdlet): Updated Get-GitAutoVersion
    🧰 Build: patch
    
    📜 NOTES:
    
    ✏  AutoVersion Now returns a correct semver build version number via `[PSCustomObject]`
    
    👤 @sgkens 📅 1:01AM, Saturday 29th July 2023

--- libs/cmdlets/Get-GitAutoVersion.psm1
+++ libs/cmdlets/Get-GitAutoVersion.psm1
@@ -15,34 +15,46 @@ Function Get-GitAutoVersion {
             }
             else {
                 $gitCommits = git log -p
-                $MajorNotation = [regex]::Matches($gitCommits, "Build: Major", [RegexOptions]::IgnoreCase)
-                $MinorNotation = [regex]::Matches($gitCommits, "Build: Minor", [RegexOptions]::IgnoreCase)
-                $PatchNotation = [regex]::Matches($gitCommits, "Build: Patch", [RegexOptions]::IgnoreCase)
 
-                [int]$majorTags = $MajorNotation.Count -1
-                [int]$minorTags = $MinorNotation.Count -1
-                [int]$patchTags = $PatchNotation.Count -1
-                if ($MajorNotation.count -ne 1) {
-                    foreach($tag in $MajorNotation){
+                for($l=$gitcommits.count; $l -gt 0; $l--) {
+                    if ([regex]::Matches($gitCommits[$l], "Build: Patch", [RegexOptions]::IgnoreCase)) {
+                        $patch++
+                    }
+                    if ([regex]::Matches($gitCommits[$l], "Build: Minor", [RegexOptions]::IgnoreCase)) {
+                        $patch=0
+                        $minor++
+                    }
+                    if ([regex]::Matches($gitCommits[$l], "Build: Major", [RegexOptions]::IgnoreCase)) {
+                        $patch=0
+                        $minor=0
                         $major++
                     }
                 }
-                if ($MinorNotation.count -ne 1 ) {
-                    foreach($tag in $MinorNotation){
-                        $minor++
-                    }
-                }
-                if ($PatchNotation.count -ne 1) {
-                    foreach($tag in $PatchNotation){
-                        $patch++
-                    }
-                }
-                if ($major -eq 0 -and $minor -eq 0 -and $patch -eq 0) {
-                    $minor = 1
-                }
+                # $MajorNotation = [regex]::Matches($gitCommits, "Build: Major", [RegexOptions]::IgnoreCase)
+                # $MinorNotation = [regex]::Matches($gitCommits, "Build: Minor", [RegexOptions]::IgnoreCase)
+                # $PatchNotation = [regex]::Matches($gitCommits, "Build: Patch", [RegexOptions]::IgnoreCase)
+
+                # [int]$majorTags = $MajorNotation.Count -1 # regex will always return 1 match so we need to subtract 1
+                # [int]$minorTags = $MinorNotation.Count -1 # --
+                # [int]$patchTags = $PatchNotation.Count -1 # --
+                # if ($patchTags -ne 0) {
+                #     for ($pt = $patchTags; $mn -gt 0; $pt--) { $Patch++ }
+                #     if ($minorTags -ne 0 ) {
+                #         for ($mit = $minorTags; $mit -gt 0; $mit--) { $Minor++ }
+                #         $patch = 0
+                #     }
+                #     if ($majorTags -ne 0) {
+                #         for ($mt = $majortags; $mt -gt 0; $mt--) { $major++ }
+                #         $minor = 0
+                #         $patch = 0
+                #     }
+                # }
+                # if ($major -eq 0 -and $minor -eq 0 -and $patch -eq 0) {
+                #     $minor = 1
+                # }
                 return [PSCustomObject]@{ 
                     NewVersion="$major.$minor.$patch";
-                    PreVersion="$($majorTags.tostring()).$($minorTags.tostring()).$($patchTags.tostring())" 
+                    ParsedLines = "$($gitCommits.count.tostring())" 
                 }
             }
         }
