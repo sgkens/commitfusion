2a3a46514d50c1ee9da6c2a09837edeaa9050be3
sgkens <sgkens@lab.davilion.online>
2023-12-02 03:04:54 +1100

    👷 ci(build): updating gitlab ci/cd
    🧰 Build:

--- build/bt1-p2-ci-test-pssa.ps1
+++ build/bt1-p2-ci-test-pssa.ps1
@@ -1,1 +1,3 @@
-Invoke-ScriptAnalyzer -Path ..\dist\CommitFusion\ -Recurse -severity warning -excluderule PSUseBOMForUnicodeEncodedFile,PSAvoidUsingWriteHost
+find-module -name PSScriptAnalyzer -Repository PSGallery | install-module -Scope CurrentUser -Force -SkipPublisherCheck
+Import-Module -Name PSScriptAnalyzer   
+Invoke-ScriptAnalyzer -Path '.\dist\CommitFusion' -Recurse -severity warning -excluderule PSUseBOMForUnicodeEncodedFile,PSAvoidUsingWriteHost
