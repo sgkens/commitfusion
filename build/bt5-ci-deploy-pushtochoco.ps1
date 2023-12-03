a789885ca04e4503dfefb1e7977de2db0c89af64
sgkens <sgkens@lab.davilion.online>
2023-12-04 09:55:16 +1100

    👷 ci(build): updating gitlab ci/cd
    🧰 Build:

--- build/bt5-ci-deploy-pushtochoco.ps1
+++ build/bt5-ci-deploy-pushtochoco.ps1
@@ -10,8 +10,7 @@ $SemVerVersion        = $ModuleManifest.Version -replace "\.\d+$",""
 if(Get-command choco.exe){
   write-host "Chocolatey is installed, skipping install"
   write-host "Pushing to chocolatey https://community.chocolatey.org/"
-  choco apikey --key $CHOCO_API_KEY --source https://push.chocolatey.org/
-  choco.exe push .\dist\$ModuleName.$SemVerVersion.nupkg --source 'https://community.chocolatey.org/'
+  choco push .\dist\choco\$($ModuleManifest.CompanyName).$ModuleName.$SemVerVersion.nupkg --source 'https://community.chocolatey.org/' --api-key $CHOCO_API_KEY --source https://push.chocolatey.org/
   write-host "Pushed to chocolatey - Complete"
 }else{
   write-host "Chocolatey is not installed, please install chocolatey https://community.chocolatey.org/"
